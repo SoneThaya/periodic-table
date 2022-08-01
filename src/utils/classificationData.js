877e241019bc6d3af4993e0cc55ac2f425a53180
sone <sthaya@gmail.com>
2022-07-31 18:01:15 -0700

    added filter by classification buttons and screen shot of table

---
+++ src/utils/classificationData.js
@@ -0,0 +1,11 @@
+export const classificationData = [
+  "Noble_gases",
+  "Reactive_nonmetals",
+  "Metalloids",
+  "Post-transition_metals",
+  "Transition_metals",
+  "Lanthanoids",
+  "Actinoids",
+  "Alkali_earth_metals",
+  "Alkali_metals",
+];
