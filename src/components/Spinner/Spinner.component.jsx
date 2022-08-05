bfb71517d950980fead0e10474a56654bc8ff56f
sone <sthaya@gmail.com>
2022-08-04 19:09:20 -0700

    added react spinners and passed it down to render component

--- src/components/Spinner/Spinner.component.jsx
+++ src/components/Spinner/Spinner.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Spinner.styles.css";
 
 const Spinner = ({ props }) => {
-  
   return (
     <div className="spinner-container">
       <div className={`loading-spinner ${props}`}></div>
